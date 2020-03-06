fbbea586d2229db871cad9c7e35c07118103a626
Michah McKinney <michah.mckinney@gmail.com>
2020-03-06 14:38:22 -0600

    adding for loop in genre.html and comments

--- server.py
+++ server.py
@@ -2,18 +2,29 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 genres = {
-	"horror" : [],
+	"horror" : [
+		{
+			"name": "It",
+			"locations": []
+		},
+		{
+			"name": "Doctor Sleep",
+			"locations": []
+		}
+	],
 	"action" : [],
 	"romance" : []
 }
-
+#data structure holds movies
 @app.route('/')
 def index():
 	return render_template('index.html', genres=genres.keys())
-
+#route to home page from index for genre select element
 @app.route('/genre/<g>')
 def genre(g=None):
-	return render_template('genre.html')
+	print(genres.get(g))
+	return render_template('genre.html', genre=g)
+#route to the data in genres, prints genre selected and returns genre.html template with incerted data
 
 
 if __name__ == "__main__":
